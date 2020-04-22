7ce9ed01e01e1beb6e1c1005325aa5e46bcbea89
unknown <carter53202@gmail.com>
2020-04-22 18:09:31 -0500

    updated information in the day class. Started to make a method to add people to the customer list.

--- Day.cs
+++ Day.cs
@@ -11,12 +11,16 @@ namespace LemonadeStand_3DayStarter
         //Member variables
         public Weather dailyWeather;
         public List<Customer> customers;
+        public Weather dailyTemperature; 
 
         //Constructor
         public Day()
         {
             dailyWeather = new Weather();
             customers = new List<Customer>();
+            Customer customer = new Customer();
+
+
         }
 
 
@@ -26,10 +30,27 @@ namespace LemonadeStand_3DayStarter
         public void DisplayWeather()
         {
             Console.WriteLine("Weather:" + dailyWeather.dailyCondition + "and " + dailyWeather.dailyTemperature);
-            
+
         }
 
         //Make a method that will use some if/else if to add Customers to the customers list -- add more if the weather is nicer!
+        public void AddCustomers()
+        {
+            if (dailyWeather.dailyTemperature >= 75)
+            {
+                customers.Add(Customer);
+
+            }
+                
+                    
+                    
+                    
+                    
+                
+       
+
+
+
         //What would it look like to add one single Customer to the list? Can you use a loop do this multiple times?
 
     }
